5107ddbc8904db39b465b165ef5574479faaba5e
Sandeep Jadam <withsj.in@gmail.com>
2021-04-10 19:35:57 +0530

    loop using c

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import ctypes
+from time import time
 
 math = ctypes.CDLL("./math.so")
 
-a = math.add(1,2)
+print("\n#Use add function")
+print("Add number ",math.add(1,2))
 
-print(a)
+print("\n#Use loopFmul and cal time it takes")
+S = time()
+
+print("loops : ",math.loopFmul(1000000))
+
+E = time()
+
+print("Time in sec: ",E-S)
+
+
+
+
--- math.c
+++ math.c
@@ -2,3 +2,13 @@ int add(int a,int b)
 {
     return a+b;
 }
+
+float loopFmul(int limit)
+{
+    int total= 1;
+    
+    for (int i = 0; i < limit; i++)
+    {total *= i;}
+
+    return total;
+}
